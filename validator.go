74c1d1551b9ff18465af533ca2ba8e7499e6de06
ns-cn <ns-cn@qq.com>
2022-12-16 15:49:52 +0800

    Validator

--- validator.go
+++ validator.go
@@ -3,8 +3,8 @@ package goter
 import "os"
 
 func Validate[T interface{}](data T, checker func(T) bool, message string) {
-	if !checker(data) {
-		_, _ = os.Stderr.WriteString(message)
+	if checker(data) {
+		_, _ = os.Stderr.WriteString(message + "\n")
 		os.Exit(1)
 	}
 }
