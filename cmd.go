58ff8d579203bd01da6fdaccfbd9a48f996edc54
ns-cn <ns-cn@qq.com>
2022-12-26 09:44:54 +0800

    common command

---
+++ cmd.go
@@ -0,0 +1,45 @@
+package goter
+
+import (
+	"github.com/spf13/cobra"
+)
+
+// NewRootCmd 创建根命令，包含一个根命令以及根命令下的版本信息查看命令
+func NewRootCmd(use, short, version string) *Command {
+	root := newRootCmd(use, short)
+	commandVersion := newVersionCmd(use, version)
+	root.AddCommand(commandVersion)
+	return root
+}
+
+// CmdStringFlag 命令的字符串参数
+type CmdStringFlag struct {
+	P               *string
+	Name, Shorthand string
+	Value           string
+	Usage           string
+}
+
+// Command 命令的包装类
+type Command struct {
+	Cmd *cobra.Command
+}
+
+// Bind 绑定指定的参数，可指定具体的字符串参数
+func (c *Command) Bind(flags ...CmdStringFlag) *Command {
+	for _, flag := range flags {
+		c.Cmd.Flags().StringVarP(flag.P, flag.Name, flag.Shorthand, flag.Value, flag.Usage)
+	}
+	return c
+}
+
+// AddCommand 为当前命令添加子命令
+func (c *Command) AddCommand(subCommand *Command) *Command {
+	c.Cmd.AddCommand(subCommand.Cmd)
+	return c
+}
+
+// 执行当前命令
+func (c *Command) Execute() error {
+	return c.Cmd.Execute()
+}
--- cmd_common.go
+++ cmd_common.go
@@ -6,21 +6,25 @@ import (
 	"runtime"
 )
 
-func Root(use, short, version string) *cobra.Command {
-	root := &cobra.Command{
+// newRootCmd 创建一个根命令
+func newRootCmd(use, short string) *Command {
+	return &Command{Cmd: &cobra.Command{
 		Use:   use,
 		Short: short,
 		Run: func(cmd *cobra.Command, args []string) {
 			_ = cmd.Help()
 		},
-	}
-	root.AddCommand(&cobra.Command{
+	}}
+}
+
+// newVersionCmd 创建一个版本信息查看命令
+func newVersionCmd(use, version string) *Command {
+	return &Command{Cmd: &cobra.Command{
 		Use:     "version",
 		Aliases: []string{"v"},
 		Short:   "打印当前版本号",
 		Run: func(cmd *cobra.Command, args []string) {
 			fmt.Println(fmt.Sprintf("%s version: v%s(%s/%s)", use, version, runtime.GOOS, runtime.GOARCH))
 		},
-	})
-	return root
+	}}
 }
