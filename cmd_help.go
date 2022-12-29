bf55f07cf5cefd4f8010583c9e72889af8d6d53e
ns-cn <ns-cn@qq.com>
2022-12-29 13:41:05 +0800

    pointer to Value

--- cmd_help.go
+++ cmd_help.go
@@ -7,20 +7,22 @@ import (
 )
 
 // 环境变量，是否打印当前版本号
-var envVersionShowRuntime = false
+var envVersionShowRuntime = NewCmdFlagBool(false, "runtime", "R", "show runtime or not")
 
 // newVersionCmd 创建一个版本信息查看命令
 func newVersionCmd(use, version string) *Command {
-	return (&Command{Cmd: &cobra.Command{
+	cmdVersion := Command{Cmd: &cobra.Command{
 		Use:     "version",
 		Aliases: []string{"v"},
 		Short:   "打印当前版本号",
 		Run: func(cmd *cobra.Command, args []string) {
-			if envVersionShowRuntime {
+			if envVersionShowRuntime.Value {
 				fmt.Println(fmt.Sprintf("%s version: v%s(%s/%s)", use, version, runtime.GOOS, runtime.GOARCH))
 			} else {
 				fmt.Println(fmt.Sprintf("%s version: v%s", use, version))
 			}
 		},
-	}}).Bind(NewCmdFlagBool(false, "runtime", "R", "show runtime or not"))
+	}}
+	cmdVersion.Bind(&envVersionShowRuntime)
+	return &cmdVersion
 }
