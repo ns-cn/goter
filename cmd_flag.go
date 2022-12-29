75944d5363c5618c2636811dd46ae7b029972ede
ns-cn <ns-cn@qq.com>
2022-12-29 09:26:16 +0800

    cmd flag int

--- cmd_flag.go
+++ cmd_flag.go
@@ -3,7 +3,23 @@ package goter
 import "github.com/spf13/cobra"
 
 type CmdFlag interface {
+	// Bind 命令参数的绑定方式声明
 	Bind(c *cobra.Command)
+}
+
+// NewCmdFlagString 创建新的命令行字符串参数
+func NewCmdFlagString(p *string, value string, name string, shorthand string, usage string) CmdFlagString {
+	return CmdFlagString{P: p, Name: name, Shorthand: shorthand, Usage: usage, Value: value}
+}
+
+// NewCmdFlagBool 创建新的命令行bool参数
+func NewCmdFlagBool(p *bool, value bool, name string, shorthand string, usage string) CmdFlagBool {
+	return CmdFlagBool{P: p, Name: name, Shorthand: shorthand, Usage: usage, Value: value}
+}
+
+// NewCmdFlagInt 创建新的命令行int参数
+func NewCmdFlagInt(p *int, defaultValue int, name string, shorthand string, usage string) CmdFlagInt {
+	return CmdFlagInt{P: p, Name: name, Shorthand: shorthand, Usage: usage, Value: defaultValue}
 }
 
 // CmdFlagString 命令的字符串参数
@@ -12,9 +28,8 @@ type CmdFlagString struct {
 	Name      string
 	Shorthand string
 	Usage     string
-
-	P     *string
-	Value string
+	P         *string
+	Value     string
 }
 
 func (f CmdFlagString) Bind(c *cobra.Command) {
@@ -33,4 +48,18 @@ type CmdFlagBool struct {
 
 func (f CmdFlagBool) Bind(c *cobra.Command) {
 	c.Flags().BoolVarP(f.P, f.Name, f.Shorthand, f.Value, f.Usage)
+}
+
+// CmdFlagInt 命令的int类型参数
+type CmdFlagInt struct {
+	CmdFlag
+	Name      string
+	Shorthand string
+	Usage     string
+	P         *int
+	Value     int
+}
+
+func (f CmdFlagInt) Bind(c *cobra.Command) {
+	c.Flags().IntVarP(f.P, f.Name, f.Shorthand, f.Value, f.Usage)
 }
