c6e5cc8023f15ca6543d868f27b95b09374e0928
ns-cn <ns-cn@qq.com>
2023-01-06 13:20:21 +0800

    Command.NewSubCommand

--- cmd.go
+++ cmd.go
@@ -35,3 +35,12 @@ func (c *Command) AddCommand(subCommand *Command) *Command {
 func (c *Command) Execute() error {
 	return c.Cmd.Execute()
 }
+
+// NewSubCommand 针对当前命令创建子命令：返回新创建命令
+func (c *Command) NewSubCommand(sub *cobra.Command) *Command {
+	subCommand := &Command{
+		Cmd: sub,
+	}
+	c.AddCommand(subCommand)
+	return subCommand
+}
