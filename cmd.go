58e3fe1953234051a29660b54b073986180b4a64
ns-cn <ns-cn@qq.com>
2023-01-09 16:42:16 +0800

    new command with flags

--- cmd.go
+++ cmd.go
@@ -5,18 +5,20 @@ import (
 )
 
 // NewRootCmd 创建根命令，包含一个根命令以及根命令下的版本信息查看命令
-func NewRootCmd(use, short, version string) *Command {
+func NewRootCmd(use, short, version string, flags ...CmdFlagBinder) *Command {
 	root := newRootCmd(use, short, nil)
 	commandVersion := newVersionCmd(use, version)
 	root.AddCommand(commandVersion)
+	root.Bind(flags...)
 	return root
 }
 
 // NewRootCmdWithAction 自定义命令执行内容的根目录创建
-func NewRootCmdWithAction(use, short, version string, action func(*cobra.Command, []string)) *Command {
+func NewRootCmdWithAction(use, short, version string, action func(*cobra.Command, []string), flags ...CmdFlagBinder) *Command {
 	root := newRootCmd(use, short, action)
 	commandVersion := newVersionCmd(use, version)
 	root.AddCommand(commandVersion)
+	root.Bind(flags...)
 	return root
 }
 
@@ -45,10 +47,11 @@ func (c *Command) Execute() error {
 }
 
 // NewSubCommand 针对当前命令创建子命令：返回新创建命令
-func (c *Command) NewSubCommand(sub *cobra.Command) *Command {
+func (c *Command) NewSubCommand(sub *cobra.Command, flags ...CmdFlagBinder) *Command {
 	subCommand := &Command{
 		Cmd: sub,
 	}
+	subCommand.Bind(flags...)
 	c.AddCommand(subCommand)
 	return subCommand
 }
